4fbefcef53069f6a2c7c648fe6d922443ae8d00a
Bohdan Khorolets <bogdan@khorolets.com>
2024-06-04 15:15:42 +0300

    Handle region

--- indexer/src/configs.rs
+++ indexer/src/configs.rs
@@ -1,3 +1,4 @@
+use aws_sdk_s3::config::Region;
 use clap::{Parser, Subcommand};
 use tracing_subscriber::EnvFilter;
 
@@ -111,6 +112,11 @@ impl Opts {
             ChainId::Betanet(_) => config_builder.betanet(),
             ChainId::Custom(_) => {
                 let s3_conf = aws_sdk_s3::config::Builder::default()
+                    .region(Region::new(
+                        std::env::var("S3_REGION_NAME")
+                            .expect("S3_REGION_NAME env variable is not set")
+                            .to_string(),
+                    ))
                     .endpoint_url(
                         std::env::var("S3_ENDPOINT")
                             .unwrap_or("https://s3.amazonaws.com".to_string()),
